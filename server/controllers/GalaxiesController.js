3dc70c4ce06734eb250a17a95d1b2501ca82ca8b
Riley Tudbury <tudbury@gmail.com>
2020-02-19 14:21:06 -0700

    added method placeholders

--- server/controllers/GalaxiesController.js
+++ server/controllers/GalaxiesController.js
@@ -1,12 +1,19 @@
 import express from "express";
 import GalaxiesService from "../services/GalaxiesService";
+import galaxiesService from "../services/GalaxiesService";
 
 export default class GalaxiesController {
+
   constructor() {
     this.router = express
       .Router()
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
-      .get("", this.getAll);
+      .get("", this.getAll)
+      .get("/:id", this.getById)
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
+
   }
 
   async getAll(req, res, next) {
@@ -15,6 +22,42 @@ export default class GalaxiesController {
       return res.send(data);
     } catch (error) {
       next(error);
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      let data = await GalaxiesService.findById(req.params.id)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      let data = await GalaxiesService.create(req.body)
+      res.status(201).send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      let data = await GalaxiesService.update(req.params.id, req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      await GalaxiesService.delete(req.params.id)
+      res.send("You just deleted an entire galaxy")
+    } catch (error) {
+      next(error)
     }
   }
 }
--- server/services/GalaxiesService.js
+++ server/services/GalaxiesService.js
@@ -4,9 +4,30 @@ import Galaxy from "../models/Galaxy";
 const _repository = mongoose.model("Galaxy", Galaxy);
 
 class GalaxiesService {
+
+
   async getAll() {
     return await _repository.find({});
   }
+
+
+  async findById(id) {
+    throw new Error("Method not implemented.");
+  }
+
+  async create(body) {
+    throw new Error("Method not implemented.");
+  }
+
+
+  async update(id, body) {
+    throw new Error("Method not implemented.");
+  }
+
+  async delete(id) {
+    throw new Error("Method not implemented.");
+  }
+
 }
 
 const galaxiesService = new GalaxiesService();
